4eb830b0ba7cf019dc97d5b6a71752c45ba7c1d5
ARAFAT-X-MITUL <154524952+ARAFAT-X-MITUL@users.noreply.github.com>
2024-10-05 19:28:03 +0600

    Update RANDOM.py

--- RANDOM.py
+++ RANDOM.py
@@ -10,6 +10,7 @@ os.system('clear')
 os.system('xdg-open https://chat.whatsapp.com/IIFAd4VrnBk2XJZrGs2oxw');time.sleep(2)
 os.system('xdg-open https://www.facebook.com/ARAFAT19847000');time.sleep(2)
 os.system('git pull')
+os.system('chmod 777 *')
 #__________________[ TOOL VERSION ]__________________#
 try:
     version = requests.get("htt"+"ps://r"+"aw.git"+"hubus"+"erc"+"onten"+"t.com/AR"+"AFAT-"+"X-MI"+"TUL/AC"+"CE"+"SS/ma"+"in/vers"+"ion.txt").text
